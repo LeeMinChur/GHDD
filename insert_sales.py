9135df67005a387df9e069829fb97b62764a4826
Jang <dbseod7@naver.com>
2020-03-10 16:40:21 +0900

    reup

--- insert_sales.py
+++ insert_sales.py
@@ -3,7 +3,6 @@ def insert_sales():
     db = pymysql.connect(host='192.168.0.7', port=3306, user='root', passwd='5284', db='Project', charset='utf8')
     Cursor = db.cursor()
     sql="""insert into menu(menu_num,menu_name,menu_price,menu_recipe1,menu_recipe2,menu_recipe3) values (%s, %s, %s, %s, %s, %s)"""
-    sql1="""update menu set menu.menu_name = @cnt:=@cnt+1;"""
     mn_num=input()
     mn_name=input()
     mn_price=input()
@@ -12,7 +11,6 @@ def insert_sales():
     mn_recipe3 = input()
     data=(mn_num,mn_name,mn_price,mn_recipe1,mn_recipe2,mn_recipe3)
     Cursor.execute(sql,data)
-    Cursor.execute(sql1)
 
     db.commit()
     db.close()
