5c0159a08249c0694f034cd20503fb2ba3740c6f
Jang <dbseod7@naver.com>
2020-03-11 10:25:01 +0900

    insert,delete,update

---
+++ insert_menu.py
@@ -0,0 +1,19 @@
+import pymysql
+def insert_menu():
+    db = pymysql.connect(host='192.168.0.7', port=3306, user='root', passwd='5284', db='Project', charset='utf8')
+    menu_ins_cursor = db.cursor()
+    ins_menu_sql="insert into menu values (%s,%s,%s,%s,%s)"
+    mn_name=input()
+    mn_price=input()
+    mn_recipe1=input()
+
+    mn_recipe2 = input()
+    mn_recipe3 = input()
+    data=(mn_name,mn_price,mn_recipe1,mn_recipe2,mn_recipe3)
+    menu_ins_cursor.execute(ins_menu_sql,data)
+
+    db.commit()
+    db.close()
+
+insert_menu()
+
