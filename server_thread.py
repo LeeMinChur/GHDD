8a2d2f421c0c9c0dd7c68e099e3856d28b03bf94
MSG <ever9946@gmail.com>
2020-03-11 22:29:29 +0900

    server_tts&thread

--- server_thread.py
+++ server_thread.py
@@ -1,25 +1,23 @@
 #-*- coding: utf-8 -*-
-from socket import *
+import socket
 import threading
+import argparse
 import time
 import Adafruit_SSD1306
 import RPi.GPIO as GPIO
 from PIL import Image, ImageDraw, ImageFont
-
-#disp=Adafruit_SSD1306.SSD1306_128_64(rst=None, i2c_address=0x3C)
-#disp.begin()
-#disp.clear()
-#disp.display()
-#width=disp.width
-#height=disp.height
-#image=Image.new('1',(width,height))
-#draw=ImageDraw.Draw(image)
-#padding=-2
-#top=padding
-#botton=height-padding
-#x=0
-#font=ImageFont.load_default()
-
+import pygame
+user_list={}
+notice_flag=0
+freq=24000
+bitsize=-16
+channels=1
+sbuffer=2048
+ordercheckmp3="ordercheck.mp3"
+orderconfirmmp3="ordercomfirm.mp3"
+orderdeniedmp3="orderdenied.mp3"
+HOST=''
+PORT=8888
 btnOrder=14
 btnCancel=15
 GPIO.setwarnings(False)
@@ -27,58 +25,78 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setup(btnOrder,GPIO.IN,pull_up_down=GPIO.PUD_DOWN)
 GPIO.setup(btnCancel,GPIO.IN,pull_up_down=GPIO.PUD_DOWN)
 global r_flag
-
 def send(sock):
     try:
         while True:
             if GPIO.input(btnOrder)==GPIO.HIGH :
                 sock.send('주문접수'.encode('utf-8'))
                 print('send:주문접수')
+                pygame.mixer.init(freq,bitsize,channels,sbuffer)
+                pygame.mixer.music.load(orderconfirmmp3)
+                pygame.mixer.music.set_volume(0.1)
+                pygame.mixer.music.play()
+                time.sleep(0.5)
                 r_flag=0
-                break
             elif GPIO.input(btnCancel)==GPIO.HIGH:
-                sock.send('주문취소'.encode('utf-8'))
+                sock.send('주문거절'.encode('utf-8'))
                 print('send:주문거절')
+                pygame.mixer.init(freq,bitsize,channels,sbuffer)
+                pygame.mixer.music.load(orderdeniedmp3)
+                pygame.mixer.music.set_volume(0.1)
+                pygame.mixer.music.play()
                 r_flag=0
-                break
-    except:
+                time.sleep(0.5)
+            time.sleep(0.01)
+    except Exception as e:
+        print(e)
         pass
 def receive(sock):
     try:
         while True:
             recvData = sock.recv(1024)
             if recvData.decode('utf-8')=='주문완료':
-                #draw.text((x,top),'order',font=font,fill=255)
                 print('receive:주문완료')
-                break
+                pygame.mixer.init(freq,bitsize,channels,sbuffer)
+                pygame.mixer.music.load(ordercheckmp3)
+                pygame.mixer.music.set_volume(0.1)
+                pygame.mixer.music.play()
             elif recvData.decode('utf-8')=='주문취소':
                 print('receive:주문취소')
                 r_flag=0
-                break
     except:
         pass
-HOST=''
-PORT=8888
 
-serverSock=socket(AF_INET,SOCK_STREAM)
-serverSock.bind((HOST,PORT))
+serverSock=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+print("socket created!")
+try:
+    serverSock.bind((HOST,PORT))
+except socket.error:
+    print('bind failed')
 serverSock.listen(5)
-conn,addr = serverSock.accept()
-while True:
-    try:
-        sender = threading.Thread(target=send,args=(conn,))
-        receiver=threading.Thread(target=receive,args=(conn,))
+#print('succes!')
+#conn,addr = serverSock.accept()
+#print('connected')
+#print(conn)
+def wheh():
+    conn,addr=serverSock.accept()
+    t=threading.Thread(target=wheh)
+    sender=threading.Thread(target=send,args=(conn,))
+    receiver=threading.Thread(target=receive,args=(conn,))
+    t.start()
+    while True:
+        print('connect:', addr[0],addr[1])
         sender.start()
         receiver.start()
         sender.join()
         receiver.join()
-        #image=Image.new('1',(width,height))
-        #draw=ImageDraw.Draw(image)
-        #disp.image(image)
-        #disp.display()
         time.sleep(0.5)
+while True:
+    try:
+        wheh()
     except KeyboardInterrupt as e:
         print(e)
+        serverSock.close()
+        conn.close()
         break
 
     
