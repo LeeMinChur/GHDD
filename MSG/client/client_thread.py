fbae9b310920becfc44e5a485405942da944a36b
client <client@c.c>
2020-03-12 12:24:26 +0900

    msg

--- MSG/client/client_thread.py
+++ MSG/client/client_thread.py
@@ -21,20 +21,23 @@ freq=24000
 bitsize=-16
 channels=1
 sbuffer=2048
+def mp3(source):
+    pygame.mixer.init(freq,bitsize,channels,sbuffer)
+    pygame.mixer.music.load(source)
+    pygame.mixer.music.set_volume(0.1)
+    pygame.mixer.music.play()
+
 def send(sock):
     try:
         while True:
             time.sleep(0.1)
             if GPIO.input(btnOrder)==GPIO.HIGH:
-                    sock.sendall('주문완료'.encode('utf-8'))
-                    pygame.mixer.init(freq,bitsize,channels,sbuffer)
-                    pygame.mixer.music.load(ordermp3)
-                    pygame.mixer.music.set_volume(0.1)
-                    pygame.mixer.music.play()
+                    sock.send('주문완료'.encode('utf-8'))
+                    mp3(ordermp3)
                     print('send:주문완료')
                     time.sleep(0.5)
             elif GPIO.input(btnCancel)==GPIO.HIGH:
-                    sock.sendadll('주문취소'.encode('utf-8'))
+                    sock.send('주문취소'.encode('utf-8'))
                     print('send:주문취소')
                     time.sleep(0.5)
     except:
@@ -48,17 +51,11 @@ def receive(sock):
             time.sleep(1)
             recvData = sock.recv(1024)
             if recvData.decode('utf-8') == '주문접수':
+                mp3(orderacceptmp3)
                 print('receive:주문접수')
-                pygame.mixer.init(freq,bitsize,channels,sbuffer)
-                pygame.mixer.music.load(orderacceptmp3)
-                pygame.mixer.music.set_volume(0.1)
-                pygame.mixer.music.play()
             elif recvData.decode('utf-8')=='주문거절':
+                mp3(orderdenied.mp3)
                 print('recevie:주문거절')
-                pygame.mixer.init(freq,bitsize,channels,sbuffer)
-                pygame.mixer.music.load(ordercancelmp3)
-                pygame.mixer.music.set_volume(0.1)
-                pygame.mixer.music.play()
             
     except:
         pass
