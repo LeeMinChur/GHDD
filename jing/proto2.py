329b9cbb5c0cdf2eae81cdbe0e81814e5678c138
jingrut2 <61680057+jingrut2@users.noreply.github.com>
2020-03-16 09:30:35 +0900

    proto3.py
    argument error

--- jing/proto2.py
+++ jing/proto2.py
@@ -17,7 +17,6 @@ class Ui_MainWindow(QMainWindow):
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
 
-
         self.recievecustomer_label = QtWidgets.QLabel(self.centralwidget)
         self.recievecustomer_label.setGeometry(QtCore.QRect(30, 20, 250, 31))
         font = QtGui.QFont()
@@ -27,9 +26,7 @@ class Ui_MainWindow(QMainWindow):
         self.recievecustomer_label.setFont(font)
         self.recievecustomer_label.setObjectName("recievecustomer_label")
 
-
-
-        #----------------------------------------------------------------------
+        # ----------------------------------------------------------------------
         self.customertext = QtWidgets.QTextEdit(self.centralwidget)
         self.customertext.setGeometry(QtCore.QRect(30, 60, 471, 51))
         font = QtGui.QFont()
@@ -46,7 +43,6 @@ class Ui_MainWindow(QMainWindow):
         self.receivecounterlabel.setFont(font)
         self.receivecounterlabel.setObjectName("receivecounterlabel")
 
-
         self.countertext = QtWidgets.QLineEdit(self.centralwidget)
         self.countertext.setGeometry(QtCore.QRect(30, 260, 461, 51))
         font = QtGui.QFont()
@@ -62,27 +58,22 @@ class Ui_MainWindow(QMainWindow):
         self.counterbutton.setGeometry(QtCore.QRect(30, 330, 201, 51))
         self.counterbutton.setObjectName("counterbutton")
         self.counterbutton.clicked.connect(self.pushcounter)
-        #--------------------------timer---------------------------------------#
+        # --------------------------timer---------------------------------------#
 
-        #------------------------------timerend----------------------------------#
+        # ------------------------------timerend----------------------------------#
         MainWindow.setCentralWidget(self.centralwidget)
         self.retranslateUi(MainWindow)
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
-
-
-
-
-
 
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
         MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
 
-        #-------------------------------타이머---------------------------#
-        self.i=0
-        self.j=0
-        self.k=0
-        self.l=0
+        # -------------------------------타이머---------------------------#
+        self.i = 0
+        self.j = 0
+        self.k = 0
+        self.l = 0
 
         self.timer1 = QtCore.QTimer()
         self.timer1.timeout.connect(self.guit1)
@@ -100,37 +91,36 @@ class Ui_MainWindow(QMainWindow):
         self.timer4.timeout.connect(self.guit4)
         self.timer4.start(100)
 
-
-        #----------------------------타이머--------------------------#
+        # ----------------------------타이머--------------------------#
         self.countertext.setText(str(1))
         self.receivecounterlabel.setText(_translate("MainWindow", "카운터에서 받기"))
         self.customerbutton.setText(_translate("MainWindow", "메뉴세팅과 메뉴보내기"))
         self.counterbutton.setText(_translate("MainWindow", "카운터로 메시지보내기"))
 
     def guit1(self):
-        test1= threading.Timer(0.3, self.guit1,args=[self.i])
-        self.i +=1
+        test1 = threading.Timer(0.3, self.guit1, args=[self.i])
+        self.i += 1
         self.receivecounterlabel.setText(str(self.i))
         self.receivecounterlabel.repaint()
         test1.start()
 
     def guit2(self):
-        test2= threading.Timer(0.3, self.guit2,args=[self.j])
-        self.j+=10
+        test2 = threading.Timer(0.3, self.guit2, args=[self.j])
+        self.j += 10
         self.customertext.setText(str(self.j))
         self.customertext.repaint()
         test2.start()
 
     def guit3(self):
-        test3= threading.Timer(0.3, self.guit3,args=[self.k])
-        self.k +=30
+        test3 = threading.Timer(0.3, self.guit3, args=[self.k])
+        self.k += 30
         self.recievecustomer_label.setText(str(self.k))
         self.recievecustomer_label.repaint()
         test3.start()
 
     def guit4(self):
-        test4= threading.Timer(0.3, self.guit4,args=[self.l])
-        self.l +=100
+        test4 = threading.Timer(0.3, self.guit4, args=[self.l])
+        self.l += 100
         self.countertext.setText(str(self.l))
         self.countertext.repaint()
         test4.start()
@@ -142,14 +132,10 @@ class Ui_MainWindow(QMainWindow):
         print("1")
 
 
-
-
-
-
-
 app = QtWidgets.QApplication(sys.argv)
 MainWindow = QtWidgets.QMainWindow()
 ui = Ui_MainWindow()
 ui.setupUi(MainWindow)
 MainWindow.show()
 sys.exit(app.exec_())
+
