eba510c072385dca9953a41a2dff0f6ec3728b00
msg <you@example.com>
2020-03-17 10:21:43 +0900

    1234
    1234

--- MSG/0317/0317_server.py
+++ MSG/0317/0317_server.py
@@ -22,7 +22,15 @@ class Cserver(threading.Thread):
         while True:
             get_data=self.c_socket.recv(1024)
             print('receive:%s'%get_data.decode('utf-8'))
-            put_data = get_data.decode('utf-8')
+            data = get_data.decode('utf-8')
+            if data == 'a':
+                put_data='a'
+            elif data =='b':
+                put_data='b'
+            elif data =='c':
+                put_data='c'
+            elif data =='d':
+                put_data='d'
             time.sleep(2)
                     
     def c_send(self,put_data):
@@ -46,14 +54,12 @@ s_socket.bind((host,port))
 s_socket.listen(5)
 creat_thread(s_socket)
 while True:
-   # put_data=input("입력: ")
-   # print('\n')
     if Cserver(put_data)=='1':
         break
     try:
         for i in t:
             i.c_send(put_data)
-            time.sleep(5)
+            time.sleep(0.5)
     except Exception as e:
         pass
 
