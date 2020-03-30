9bded7a03eb80043017b02a8d1634a6cccb7aa34
Hong YoungShin <hys3448@naver.com>
2020-03-30 23:05:15 +0900

    ㅋㅋ

--- YS/test_ys.py
+++ YS/test_ys.py
@@ -5,7 +5,7 @@ from RPi import GPIO
 from luma.core.interface.serial import i2c
 from luma.core.render import canvas
 from luma.oled.device import ssd1306, ssd1325, ssd1331, sh1106
-from time import sleep
+import time 
 import socket
 from PIL import ImageFont, ImageDraw
 
@@ -18,15 +18,16 @@ flg=0
 HOST = '192.168.0.2'
 PORT = 9988
 global menuindex
-names = ['아메리카노','카페모카','카페라떼','레몬에이드','초코라떼','에스프레소']
+names = []
 choice_menu=[]
 order_cnt=[]
+menu_price=[]
 for i in range(len(names)):
     order_cnt.append(0)
+now=time.localtime()
+menu_index=0
 
 
-sock=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sock.connect((HOST,PORT))
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(btn_up, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 GPIO.setup(btn_down, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
@@ -46,52 +47,59 @@ def invert(draw,x,y,text):
 def menu2(device, draw, menustr,index):
     global menuindex
     global order_cnt
-    font2 =ImageFont.truetype("/fonts/trutype/nanum/NanumBarunGothic.ttf",11)
+    font2 =ImageFont.truetype("/fonts/trutype/nanum/NanumBarunGothic.ttf",15)
     font =ImageFont.truetype("/fonts/trutype/nanum/NanumBarunGothic.ttf",14)
-    draw.rectangle(device.bounding_box, outline="white", fill="black")
-    if index <4 :
+    draw.rectangle((0,15,127,63), outline="white", fill="black")
+    draw.text((38,0),"주문확인", font=font, fill='white')
+    if index <3 :
         for i in range(len(menustr)): 
             if( i == index):
                 menuindex = i
-                invert(draw, 2, i*15, menustr[i])
-                invert(draw, 62, i*15, str(order_cnt[i]))
+                invert(draw, 2, (i*15)+15, menustr[i])
+                invert(draw, 80, (i*15)+15, str(order_cnt[i]))
             else:
-                draw.text((2, i*15), menustr[i], font=font, fill=255)
-                draw.text((62, i*15), str(order_cnt[i]), font=font, fill=255)
+                draw.text((2, (i*15)+15), menustr[i], font=font, fill=255)
+                draw.text((80, (i*15)+15), str(order_cnt[i]), font=font, fill=255)
     else:
-        for i in range(index-3,index+2):
+        for i in range(index-2,index+1):
             if (i == index):
                 menuindex = i
-                invert(draw, 2, (i-(index-3))*15, menustr[i])
-                invert(draw, 62, (i-(index-3))*15, str(order_cnt[i]))
+                invert(draw, 2, ((i-(index-2))*15+15), menustr[i])
+                invert(draw, 80, ((i-(index-2))*15+15), str(order_cnt[i]))
             elif (i==len(menustr)):
                 pass
             else:
-                draw.text((2, (i-(index-3))*15), menustr[i], font=font, fill=255)
-                draw.text((62, (i-(index-3))*15), str(order_cnt[i]), font=font, fill=255)
+                draw.text((2, ((i-(index-2))*15)+15), menustr[i], font=font, fill=255)
+                draw.text((80, ((i-(index-2))*15)+15), str(order_cnt[i]), font=font, fill=255)
         
 def menu(device, draw, menustr,index):
     global menuindex
     global order_cnt
-    font2 =ImageFont.truetype("/fonts/trutype/nanum/NanumBarunGothic.ttf",11)
+    global menu_price
+    font2 =ImageFont.truetype("/fonts/trutype/nanum/NanumBarunGothic.ttf",15)
     font =ImageFont.truetype("/fonts/trutype/nanum/NanumBarunGothic.ttf",14)
-    draw.rectangle(device.bounding_box, outline="white", fill="black")
-    if index <4 :
+    draw.rectangle((0,15,127,63), outline="white", fill="black")
+    draw.text((38,0),"메뉴목록", font=font, fill='white')
+    if index <3 :
         for i in range(len(menustr)): 
             if( i == index):
                 menuindex = i
-                invert(draw, 2, i*15, menustr[i])
+                invert(draw, 2, (i*15)+15, menustr[i])
+                invert(draw, 70, (i*15)+15, menu_price[i])
             else:
-                draw.text((2, i*15), menustr[i], font=font, fill=255)
+                draw.text((2, (i*15)+15), menustr[i], font=font, fill=255)
+                draw.text((70, (i*15)+15), menu_price[i], font=font, fill=255)
     else:
-        for i in range(index-3,index+2):
+        for i in range(index-2,index+1):
             if (i == index):
                 menuindex = i
-                invert(draw, 2, (i-(index-3))*15, menustr[i])
+                invert(draw, 2, ((i-(index-2))*15+15), menustr[i])
+                invert(draw, 70, ((i-(index-2))*15+15), menu_price[i])
             elif (i==len(menustr)):
                 pass
             else:
-                draw.text((2, (i-(index-3))*15), menustr[i], font=font, fill=255)        
+                draw.text((2, ((i-(index-2))*15)+15), menustr[i], font=font, fill=255)
+                draw.text((70, ((i-(index-2))*15)+15), menu_price[i], font=font, fill=255)          
 
 
 def sw_callback(channel):  
@@ -113,16 +121,17 @@ def sw_callback(channel):
         elif flg==1:
             with canvas(device) as draw:
                 draw.rectangle(device.bounding_box, outline="white", fill="black")
-                draw.text((38, 25), "주문 접수",font=font, fill="white")
-                order_list=','.join(choice_menu)
-                sock.send(order_list.encode('utf-8'))
+                draw.text((38, 25), "주문접수",font=font, fill="white")
+                order_list=','.join(choice_menu)+','
+ 
                 
         elif flg==2:
             with canvas(device) as draw:
                 menu(device, draw, names,0)
             flg=0
-        elif flg==3:
-            sock.send('세팅수락'.encode('utf-8'))
+
+        
+            
 
     except:
         pass
@@ -170,47 +179,81 @@ def rotary_callback2(channel):
                 menu2(device, draw, names,menuindex%len(names))
     finally:
         print("Ending")
-def test(channel):
-    sock.send('제품출고'.encode('utf-8'))
-
-def recv(sock):
+        
+def cancel(channel):
     global flg
     global choice_menu
     global order_cnt
+    font =ImageFont.truetype("/fonts/trutype/nanum/NanumBarunGothic.ttf",15)
+    with canvas(device) as draw:
+        draw.rectangle(device.bounding_box, outline="white", fill="black")
+        draw.text((43, 20), "주문이",font=font, fill="white")
+        draw.text((15, 35), "취소되었습니다", font=font,fill="white")
+        choice_menu=[]
+        order_cnt=[]
+        for i in range(len(names)):
+            order_cnt.append(0)
+        flg=2
+
+def recv(sock):
+    global flg
+    global now
+    global choice_menu
+    global order_cnt
+    global menu_price
     global names
     font =ImageFont.truetype("/fonts/trutype/nanum/NanumBarunGothic.ttf",15)
     try:
         while True:
-            sleep(0.1)
+            time.sleep(0.1)
             recv=sock.recv(1024)
-            #print(recv.decode('utf-8'))
+            print(recv.decode('utf-8'))
             if recv.decode('utf-8')=='주문완료':
-                with canvas(device) as draw:
-                    draw.rectangle(device.bounding_box, outline="white", fill="black")
-                    draw.text((20, 30), "주문접수완료",font=font, fill="white")
-                    choice_menu=[]
-                    order_cnt=[]
-                    for i in range(len(names)):
-                        order_cnt.append(0)
-                flg=2
+                if flg==1:
+                    with canvas(device) as draw:
+                        draw.rectangle(device.bounding_box, outline="white", fill="black")
+                        draw.text((25, 10), "주문접수완료",font=font, fill="white")
+                        draw.text((20, 25), "%04d/%02d/%02d" % (now.tm_year, now.tm_mon, now.tm_mday),font=font, fill="white")
+                        draw.text((30, 40), "%02d:%02d:%02d" % (now.tm_hour, now.tm_min, now.tm_sec),font=font, fill="white")
+                        choice_menu=[]
+                        order_cnt=[]
+                        for i in range(len(names)):
+                            order_cnt.append(0)
+                    flg=2
+                else:
+                    pass
             elif recv.decode('utf-8')=='메뉴세팅':
                 with canvas(device) as draw:
                     draw.rectangle(device.bounding_box, outline="white", fill="black")
-                    draw.text((10, 30), "메뉴가 변경됩니다",font=font, fill="white")
+                    draw.text((10, 30), "메뉴가변경됩니다",font=font, fill="white")
                 flg=3
             elif recv.decode('utf-8')=='세팅완료':
-                with canvas(device) as draw:
-                    draw.rectangle(device.bounding_box, outline="white", fill="black")
-                    draw.text((5, 20), "메뉴가 변경되었습니다",font=font, fill="white")
-                    draw.text((10, 35), "다시 주문해주세요",font=font, fill="white")
-                    choice_menu=[]
+                if flg==3:
+                    with canvas(device) as draw:
+                        draw.rectangle(device.bounding_box, outline="white", fill="black")
+                        draw.text((43, 8), "메뉴가",font=font, fill="white")
+                        draw.text((17, 23), "변경되었습니다", font=font,fill="white")
+                        draw.text((10, 38), "다시주문해주세요",font=font, fill="white")
+                        choice_menu=[]
+                        order_cnt=[]
+                        for i in range(len(names)):
+                            order_cnt.append(0)
+                        flg=2
+                else:
+                    with canvas(device) as draw:
+                        menu(device, draw, names,0)
+                    flg=0
                     order_cnt=[]
                     for i in range(len(names)):
                         order_cnt.append(0)
-                    flg=2        
             elif '/' in recv.decode('utf-8'):
-                names=list(recv.decode('utf-8').split(','))
+                names=list(recv.decode('utf-8').split('/'))
+                names.pop()
                 sock.send('메뉴확인'.encode('utf-8'))
+            elif '.' in recv.decode('utf-8'):
+                menu_price=list(recv.decode('utf-8').split('.'))
+                menu_price.pop()
+                sock.send('가격확인'.encode('utf-8'))
             
                 
                            
@@ -225,16 +268,15 @@ GPIO.add_event_detect(btn_up, GPIO.RISING , callback=rotary_callback1, bouncetim
 GPIO.add_event_detect(btn_down,GPIO.RISING, callback=rotary_callback2, bouncetime=250)
 GPIO.add_event_detect(btn_ok, GPIO.RISING , callback=sw_callback, bouncetime=300)
 GPIO.add_event_detect(btn_order, GPIO.RISING, callback=order_page ,bouncetime=300)
-GPIO.add_event_detect(btn_test, GPIO.RISING, callback=test ,bouncetime=300)
+GPIO.add_event_detect(btn_test, GPIO.RISING, callback=cancel ,bouncetime=300)
 
 
 
 try:
+    font =ImageFont.truetype("/fonts/trutype/nanum/NanumBarunGothic.ttf",15)
     with canvas(device) as draw:
         menu(device, draw, names,0)
-    t=Thread(target=recv,args=(sock,))
-    t.daemon = True
-    t.start()
+
     
 except KeyboardInterrupt:
     GPIO.clear()
